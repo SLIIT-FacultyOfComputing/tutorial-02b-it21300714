d84dc2f7d40215856ff2173218f302ee7a71e0fa
it21300714 <99782533+it21300714@users.noreply.github.com>
2022-03-06 18:43:39 +0000

    I have completed Tutorial

--- tute01.cpp
+++ tute01.cpp
@@ -18,13 +18,12 @@ printf("Hello World") --> std::cout << "Hello World";
 */
 
 
-#include <stdio.h>
+#include <iostream>
 int main() 
 {
-    float cm, inches;
-    printf("Enter a length in cm : ");
-    scanf("%f",&cm);
-    inches = cm / 2.54;
-    printf("Length in inches is %f \n", inches);
-    return 0;
+ float cm, inches;
+ std::cout<<"Enter a length in cm : ";
+ std::cin>>cm;
+ inches = cm / 2.54;
+ std::cout<<"Length in inches is "<<inches;
 }  
